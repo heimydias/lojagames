66f79be829a0969f0575da6d53b04328decd9778
Heimy Dias <heimysantana@hotmail.com>
2023-04-27 19:47:09 -0300

    usuario, security e service criados

---
+++ src/main/java/com/generation/lojagames/model/UsuarioLogin.java
@@ -0,0 +1,62 @@
+package com.generation.lojagames.model;
+
+import java.time.LocalDate;
+
+public class UsuarioLogin {
+	
+	private Long id;
+	private String nome;
+	private String usuario;
+	private String senha;
+	private LocalDate datanascimento;
+	private String token;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return this.senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getToken() {
+		return this.token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public LocalDate getDatanascimento() {
+		return datanascimento;
+	}
+
+	public void setDatanascimento(LocalDate datanascimento) {
+		this.datanascimento = datanascimento;
+	}
+
+}
